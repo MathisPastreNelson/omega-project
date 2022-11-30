95c09eebdef009259a0ae2bc42ccd727fc222c5e
Mathis Pastre <shiray@hotmail.fr>
2022-11-30 03:50:46 +0100

    Démarrage du projet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles/_test.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Voici le tout départ</h1>
+      </header>
+    </div >
+  );
+}
+
+export default App;
