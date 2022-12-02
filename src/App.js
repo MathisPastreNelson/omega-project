29c7bea836cbcc265724e00e903b8022bca56754
Mathis Pastre <shiray@hotmail.fr>
2022-12-02 02:29:31 +0100

    Origine du projet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./styles/_test.scss"
+
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Importation du style
+import "./styles/index.scss"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
