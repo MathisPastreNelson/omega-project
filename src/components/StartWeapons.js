678a50553c2109abfe5366ab67ca99bd83afe3fa
Mathis Pastre <shiray@hotmail.fr>
2022-12-11 04:00:29 +0100

    composant startWeapons continuité

--- src/components/StartWeapons.js
+++ src/components/StartWeapons.js
@@ -3,7 +3,13 @@ import React from 'react';
 const StartWeapons = () => {
     return (
         <div className='StartWeapons__container'>
-            <p className='test'>ceci est un test</p>
+            <div className='StartWeapons__list'>
+                <button className='weaponsButtons' type='button'>Epee</button>
+                <button className='weaponsButtons' type='button'>Hache</button>
+                <button className='weaponsButtons' type='button'>Masse</button>
+                <button className='weaponsButtons' type='button'>Arc</button>
+                <button className='weaponsButtons' type='button'>Bâton</button>
+            </div>
         </div>
     );
 };
