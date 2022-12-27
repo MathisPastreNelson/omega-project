58ff6781c9d48ef891a1165e31f18d85f6ed98f1
Mathis Pastre <shiray@hotmail.fr>
2022-12-27 04:01:44 +0100

    correction ghpage

--- src/pages/RoadSelectPage.js
+++ src/pages/RoadSelectPage.js
@@ -6,14 +6,13 @@ import RoadSelect from '../components/RoadSelect';
 import Help from '../components/Help';
 
 const RoadSelectPage = () => {
-    const [component, setComponent] = useState("roadSelect");
+    const [component] = useState("roadSelect");
     // Les points de vies maximum sont le résultat du nombre d'endurance dans le localStorage et du calcul
     const [maxHp, setMaxHp] = useState((window.localStorage.Endurance * 5) + 50);
     const [actualHp, setActualHp] = useState(maxHp)
 
     // Importation de toutes les données du localStorage
     const data = window.localStorage
-    const save = parseInt(window.localStorage.Save)
 
     return (
         <div>
